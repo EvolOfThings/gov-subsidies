545e127b65cbf5203106b72e2d9ba2940ae1d2ab
rishad <rishad.yamnoor@gmail.com>
2024-07-17 23:31:44 +0530

    subsidy and eligibility pages

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -43,24 +43,8 @@ export const Footer: FunctionComponent<FooterProps> = ({
     <div className="relative">
       <Toaster isDisplayed={isDisplayed} setIsDisplayed={setIsDisplayed} />
       <footer className="h-24 shadow-[0px_-5px_14px_0px_rgba(48,49,51,0.05)]">
-        <div className="w-full mx-auto max-w-screen-xl h-full p-4 flex items-center justify-between">
-          <a
-            target={"_blank"}
-            rel={"noreferrer"}
-            href="https://documentation.anon-aadhaar.pse.dev/docs/quick-setup"
-          >
-            <div className="flex flex-row text-sm items-center text-black font-rajdhani font-medium gap-1 hover:underline">
-              <p className="leading-none text-lg pt-1">TUTORIAL</p>
-              <Image
-                priority
-                src={externalLinkIcon}
-                width={20}
-                height={20}
-                alt="Check our tutorial"
-                className="leading-none"
-              />
-            </div>
-          </a>
+        <div className="w-full mx-auto max-w-screen-xl h-full p-4 flex items-center justify-around">
+
           <div className="flex flex-row justify-center items-center gap-8">
             <p className="text-lg text-black font-rajdhani font-medium underline">
               {shortenAddress("0x" + contractAddr?.toUpperCase())}
--- src/pages/subsidy.tsx
+++ src/pages/subsidy.tsx
@@ -1,12 +1,53 @@
 /* eslint-disable react/no-unescaped-entities */
-import { useAnonAadhaar } from "@anon-aadhaar/react";
+import { LaunchProveModal, useAnonAadhaar } from "@anon-aadhaar/react";
 import { useEffect, useContext } from "react";
 import { useRouter } from "next/router";
 import { useAccount } from "wagmi";
 import { AppContext } from "./_app";
 import { useWeb3Modal } from "@web3modal/wagmi/react";
 
-export default function Home() {
+// This is a trick to enable having both modes in under the same page.
+// This could be removed and only the <LaunchProveModal /> could be displayed.
+
+// pass fieldsToReveal to <LaunchProveModal /> to get fields
+const LaunchMode = ({
+  isTest,
+  setIsTestMode,
+  address,
+}: {
+  isTest: boolean;
+  setIsTestMode: (isTest: boolean) => void;
+  address: string;
+}) => {
+  return (
+    <span onClick={() => setIsTestMode(isTest)}>
+      <LaunchProveModal
+        nullifierSeed={Math.floor(Math.random() * 1983248)}
+        signal={address}
+        fieldsToReveal={[
+        'revealAgeAbove18',
+        'revealGender',
+        'revealPinCode',
+        'revealState',
+      ]}
+        buttonStyle={{
+          borderRadius: "8px",
+          border: "solid",
+          borderWidth: "1px",
+          boxShadow: "none",
+          fontWeight: 500,
+          borderColor: "#009A08",
+          color: "#009A08",
+          fontFamily: "rajdhani",
+        }}
+        buttonTitle={isTest ? "USE TEST CREDENTIALS" : "USE REAL CREDENTIALS"}
+        useTestAadhaar={isTest}
+      />
+    </span>
+  );
+};
+
+export default function Subsidy() {
   const [anonAadhaar] = useAnonAadhaar();
   const { setIsTestMode } = useContext(AppContext);
   const { isConnected, address } = useAccount();
@@ -14,17 +55,15 @@ export default function Home() {
   const router = useRouter();
 
   useEffect(() => {
-
+    // if (anonAadhaar.status === "logged-in") {
+    //   router.push("./vote");
+    // }
     if(isConnected){
       router.push('/')
     }  else if(isConnected && anonAadhaar.status === "logged-in"){
-      router.push('/eligibility')
+      router.push('/')
     }
-  }, [anonAadhaar, router, isConnected]);
-
-  const handleQRModal = () => {
-    router.push('/subsidy');
-  }
+  }, [anonAadhaar, router]);
 
  
   return (
@@ -49,12 +88,25 @@ export default function Home() {
             {isConnected ? (
               <div>
                 <div className="flex gap-4 place-content-center">
-              <button
+                  {/* <LaunchMode
+                    isTest={false}
+                    setIsTestMode={setIsTestMode}
+                    address={address as string}
+                  /> */}
+
+                  {/* TODO: this will go into dirrefct page, after getting the proof and selecting the scheme */}
+                  <LaunchMode
+                    isTest={true}
+                    setIsTestMode={setIsTestMode}
+                    address={address as string}
+                  />
+
+              {/* <button
                 className="bg-[#009A08] rounded-lg text-white px-6 py-1 font-rajdhani font-medium"
-                onClick={() => handleQRModal}
+                onClick={() => open()}
               >
-                Continue to upload Aadhar QR code
-              </button>
+                Explore
+              </button> */}
 
 
                 </div>
@@ -66,7 +118,7 @@ export default function Home() {
                 className="bg-[#009A08] rounded-lg text-white px-6 py-1 font-rajdhani font-medium"
                 onClick={() => open()}
               >
-                CONNECT WALLET
+                YOU MUST BE CONNECTED TO CONTINUE
               </button>
             )}
           </div>
