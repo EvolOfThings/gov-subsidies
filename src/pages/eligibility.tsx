a7b5a1f13b12e00340c561accb116f11b5705a7b
rishad <rishad.yamnoor@gmail.com>
2024-07-18 21:18:20 +0530

    aadhar byte arr to string

--- src/pages/eligibility.tsx
+++ src/pages/eligibility.tsx
@@ -1,19 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  convertBigIntToByteArray,
+  decompressByteArray,
+  deserialize,
+} from "@anon-aadhaar/core";
+import { useAnonAadhaar } from "@anon-aadhaar/react";
+import { useAccount } from "wagmi";
+import { useEffect } from "react";
 import SubsidiesEligibility from '../components/SubsidiesEligibility';
 import { EligibilityCriteria } from '../subsidyData';
 
-const userCriteria: EligibilityCriteria = {
-  ageAbove18: true,
-  gender: 'female',
-  state: 'Sikkim',
+const Eligibility: React.FC = () => {
+  const [anonAadhaar] = useAnonAadhaar();
+  const { isConnected, address } = useAccount();
+  const [userAadhaarData, setUserAadhaarData] = useState<EligibilityCriteria>();
+  const [aadhaarState, setAadhaarState] = useState<string>("");
+
+ 
+   useEffect(() => {
+    const aaObj = localStorage.getItem("anonAadhaar");
+    const anonAadhaarProofs = JSON.parse(aaObj!).anonAadhaarProofs;
+
+    deserialize(
+      anonAadhaarProofs[Object.keys(anonAadhaarProofs).length - 1].pcd
+    ).then((result) => {
+      console.log("anonAadresulthaar", result);
+
+      const byteGenderData = convertBigIntToByteArray(BigInt(result.proof.gender));
+      const charConversionGender = String.fromCharCode(...byteGenderData);
+
+      //covert  to byte array and then reverse(the protocol needs to come up with a util func)
+      const byteStateData = convertBigIntToByteArray(BigInt(result.proof.state));
+      const charConversionState = String.fromCharCode(...byteStateData);
+      const reversedStringState = charConversionState.split("").reverse().join("");
+
+      setAadhaarState(reversedStringState);
+      setUserAadhaarData({
+        ageAbove18: result.proof.ageAbove18 === "1" && true,
+        gender: charConversionGender  === "M" ? "M" : charConversionGender === "F" ? "F" : "O",
+        state: reversedStringState,
+      });
+  
+    });
+  }, [anonAadhaar]);
+
+return (
+  <div className="min-h-screen flex items-center justify-center bg-gray-100">
+
+    {isConnected && userAadhaarData && (
+      <SubsidiesEligibility state={aadhaarState} userCriteria={userAadhaarData} />
+    )}
+
+    {!isConnected && (
+      <button
+        disabled={true}
+        type="button"
+        className="rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300"
+      >
+        You need to connect your wallet first ⬆️
+      </button>
+    )}
+  </div>
+);
 };
 
-const App: React.FC = () => {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <SubsidiesEligibility state="Sikkim" userCriteria={userCriteria} />
-    </div>
-  );
-};
-
-export default App;
+export default Eligibility;
